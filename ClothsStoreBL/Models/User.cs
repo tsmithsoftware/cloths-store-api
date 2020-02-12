d84598189e28b6e166f17a52bb4bd001ccbae95a
tsmithsoftware <tijak@hotmail.com>
2020-02-12 19:02:27 +0000

    Add Uuser model, update .gitignore

---
+++ ClothsStoreBL/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothsStore.BL.Models
+{
+    // hashed password information stored in db
+    public class User
+    {
+        public int id { get; set; }
+        public String Username { get; set; }
+        public String HashedPassword { get; set; }
+    }
+}
